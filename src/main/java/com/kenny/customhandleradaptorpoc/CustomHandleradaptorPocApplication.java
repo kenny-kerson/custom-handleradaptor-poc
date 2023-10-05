664142b431c3a5ad6dc04f0ce8378a0d6dd3dec4
kenny-kerson <kenny.kerson@gmail.com>
2023-10-05 22:14:16 +0900

    feat: Initial Commit
    - 프로젝트 최초 생성

---
+++ src/main/java/com/kenny/customhandleradaptorpoc/CustomHandleradaptorPocApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.customhandleradaptorpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomHandleradaptorPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomHandleradaptorPocApplication.class, args);
+    }
+
+}
