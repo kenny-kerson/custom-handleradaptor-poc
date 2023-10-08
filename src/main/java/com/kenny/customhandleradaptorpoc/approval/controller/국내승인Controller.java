0ddcdbfced120f62266f8d39ba1cad6ee1ed3967
kenny-kerson <kenny.kerson@gmail.com>
2023-10-08 21:07:37 +0900

    feat: 국내승인, 국내승인취소 api path 수정
    - api path와 @RequestBody가 누락된 문제 수정
    
    issue: REQ-1, REQ-3ˆ

--- src/main/java/com/kenny/customhandleradaptorpoc/approval/controller/국내승인Controller.java
+++ src/main/java/com/kenny/customhandleradaptorpoc/approval/controller/국내승인Controller.java
@@ -5,6 +5,7 @@ import com.kenny.customhandleradaptorpoc.approval.service.국내승인Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,14 +15,14 @@ public class 국내승인Controller {
 
     private final 국내승인Service domesticApprovalService;
 
-    @PostMapping
-    public 국내승인DTO.승인.Out process국내승인( final 국내승인DTO.승인.In input ) {
+    @PostMapping("/approval/domestic")
+    public 국내승인DTO.승인.Out process국내승인( @RequestBody final 국내승인DTO.승인.In input ) {
         log.debug("# 국내승인Controller process국내승인 : {}", input.toString());
         return domesticApprovalService.process국내승인(input);
     }
 
-    @PostMapping
-    public 국내승인DTO.취소.Out process국내취소( final 국내승인DTO.취소.In input ) {
+    @PostMapping("/approval/domestic/cancle")
+    public 국내승인DTO.취소.Out process국내취소( @RequestBody final 국내승인DTO.취소.In input ) {
         log.debug("# 국내승인Controller process국내취소 : {}", input.toString());
         return domesticApprovalService.process국내승인취소(input);
     }
