50f7dce44a5cdccee58d04365c314b8d7731d262
kenny-kerson <kenny.kerson@gmail.com>
2023-10-08 21:11:16 +0900

    feat: 해외승인취소 개발
    - 해외승인취소 관련 Controller, Service 메서드 추가
    - 해외승인 Controller 메서드에서 Service 호출하도록 수정
    
    issue: REQ-3, REQ-4

--- src/main/java/com/kenny/customhandleradaptorpoc/approval/controller/해외승인Controller.java
+++ src/main/java/com/kenny/customhandleradaptorpoc/approval/controller/해외승인Controller.java
@@ -18,8 +18,12 @@ public class 해외승인Controller {
     @PostMapping("/approval/oversea")
     public 해외승인DTO.승인.Out process해외승인( @RequestBody final 해외승인DTO.승인.In input ) {
         log.debug( "# 해외승인Service process해외승인 : {}", input.toString());
-        return 해외승인DTO.승인.Out.builder()
-                .결과코드("OK")
-                .build();
+        return overseaApprovalServce.process해외승인(input);
+    }
+
+    @PostMapping("/approval/oversea/cancle")
+    public 해외승인DTO.취소.Out process해외승인취소( @RequestBody final 해외승인DTO.취소.In input ) {
+        log.debug( "# 해외승인Service process해외승인취소 : {}", input.toString());
+        return overseaApprovalServce.process해외승인취소(input);
     }
 }
