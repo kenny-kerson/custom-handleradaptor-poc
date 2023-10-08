1320c679f5ef187c03a675924c33022b0e1c4ea4
kenny-kerson <kenny.kerson@gmail.com>
2023-10-08 20:47:05 +0900

    feat: 국내승인 Service 개발
    issue: REQ-1

---
+++ src/main/java/com/kenny/customhandleradaptorpoc/approval/service/dto/국내승인DTO.java
@@ -0,0 +1,46 @@
+package com.kenny.customhandleradaptorpoc.approval.service.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+public class 국내승인DTO {
+
+    public static class 승인 {
+
+        @Getter
+        @Builder
+        @ToString
+        public static class In {
+            private final String 승인번호;
+            private final String 카드번호;
+            private final String 승인금액;
+        }
+
+        @Getter
+        @Builder
+        @ToString
+        public static class Out {
+            private final String 결과코드;
+        }
+    }
+
+    public static class 취소 {
+        @Getter
+        @Builder
+        @ToString
+        public static class In {
+            private final String 승인번호;
+        }
+
+        @Getter
+        @Builder
+        @ToString
+        public static class Out {
+            private final String 결과코드;
+        }
+    }
+
+}
